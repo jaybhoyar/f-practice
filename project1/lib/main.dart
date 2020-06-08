af094cfe44ff34074ab7ad3b2891ca5a17be8465
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-06-08 21:02:25 +0530

    add func for button

--- project1/lib/main.dart
+++ project1/lib/main.dart
@@ -5,15 +5,34 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer choose');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("My first App"),
-        ),
-        body: Text("This is my default text "),
-      ),
+          appBar: AppBar(
+            title: Text("My first App"),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text("The question jhbh"),
+              RaisedButton(
+                child: Text("option 1"),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text("option 2"),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text("option 3"),
+                onPressed: answerQuestion,
+              ),
+            ],
+          )),
     );
   }
 }
