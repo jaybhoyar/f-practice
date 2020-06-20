bad785abae4d047f75fd602259383d893df7a0cd
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-06-20 14:32:51 +0530

    change network image to assest image

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -11,8 +11,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://images.squarespace-cdn.com/content/v1/5cfce28b8dc2800001e965b7/1562825261700-YJMMR31H4OJH5XTQRKOA/ke17ZwdGBToddI8pDm48kL2QOIl1zNSPYP8zvSlW8gVZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpzu5X5PffHsrpf4FRYjM8Q9Xj6BpGU7dJG1fhv13AVG82aSTbXQqLJLELP1y0rxPis/1+flutter_blog-2-750x400-1.png'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
