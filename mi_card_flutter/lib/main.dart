e030088bacc7fba781656d7e87cc2cf324755ed5
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-06-27 14:39:29 +0530

    replace padding class with ListTile

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -10,28 +10,27 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal.shade600,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 50.0,
-                    backgroundImage: AssetImage('images/profile.png'),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/profile.png'),
+                ),
+                Text(
+                  'Jay Bhoyar',
+                  style: TextStyle(
+                    fontSize: 35.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico',
                   ),
-                  Text(
-                    'Jay Bhoyar',
-                    style: TextStyle(
-                      fontSize: 35.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Pacifico',
-                    ),
-                  ),
-                  Text(
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
                     'FULL STACK DEVELOPER',
                     style: TextStyle(
                       fontSize: 16.0,
@@ -41,61 +40,46 @@ class MyApp extends StatelessWidget {
                       // fontFamily: 'Pacifico',
                     ),
                   ),
-                  Container(
-                    color: Colors.white,
-                    width: 300,
-                    margin: EdgeInsets.symmetric(vertical: 10.0),
-                    padding:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.phone,
-                          color: Colors.teal,
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        '+91 727 612 9866',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16.0,
+                          color: Colors.teal.shade900,
                         ),
-                        SizedBox(
-                          width: 15.0,
-                        ),
-                        Text(
-                          '+91 727 612 9866',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16.0,
-                            color: Colors.teal.shade900,
-                          ),
-                        )
-                      ],
+                      )),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'jaybhoyar1997@gmail.com',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16.0,
+                        color: Colors.teal.shade900,
+                      ),
                     ),
                   ),
-                  Container(
-                    color: Colors.white,
-                    width: 300,
-                    margin: EdgeInsets.symmetric(vertical: 10.0),
-                    padding:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.email,
-                          color: Colors.teal,
-                        ),
-                        SizedBox(
-                          width: 5.0,
-                        ),
-                        Text(
-                          'jaybhoyar1997@gmail.com',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16.0,
-                            color: Colors.teal.shade900,
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
