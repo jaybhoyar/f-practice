1543028ea5facdd4512b750271892bb2fe207ec5
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-07-01 16:51:43 +0530

    add both dice changes on single click

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -22,8 +22,8 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
-  var leftDiceNumber = 1;
-  var rightDiceNumber = 1;
+  var leftDiceNumber = 6;
+  var rightDiceNumber = 6;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -34,6 +34,7 @@ class _DicePageState extends State<DicePage> {
               onPressed: () {
                 setState(() {
                   leftDiceNumber = Random().nextInt(6) + 1;
+                  rightDiceNumber = Random().nextInt(6) + 1;
                 });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
@@ -43,6 +44,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
                   rightDiceNumber = Random().nextInt(6) + 1;
                 });
               },
