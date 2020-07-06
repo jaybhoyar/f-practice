0250c2996914c57444dd502d8e9cc04e677be3b9
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-07-06 15:21:07 +0530

    add rainbow buttons

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -1,17 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-import 'package:audioplayers/audioplayers.dart';
+
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: Text(nouns.first),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // ignore: missing_required_param
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  color: Colors.red,
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  color: Colors.orange,
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  color: Colors.yellow,
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  color: Colors.green,
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  color: Colors.teal,
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  color: Colors.blue,
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  color: Colors.purple,
+                  child: null,
+                ),
+              ),
+            ],
           ),
         ),
       ),
