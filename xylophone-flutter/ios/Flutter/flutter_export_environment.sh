d575fd9f0c8760c4593370c5bbb4a3263cbdd52d
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-07-06 13:57:59 +0530

    add audio package

--- xylophone-flutter/ios/Flutter/flutter_export_environment.sh
+++ xylophone-flutter/ios/Flutter/flutter_export_environment.sh
@@ -2,10 +2,12 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/jaybhoyar/Documents/flutterSDK"
 export "FLUTTER_APPLICATION_PATH=/Users/jaybhoyar/projects/flutter/all projects/xylophone-flutter"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/jaybhoyar/projects/flutter/all projects/xylophone-flutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
 export "FLUTTER_FRAMEWORK_DIR=/Users/jaybhoyar/Documents/flutterSDK/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
+export "DART_DEFINES=flutter.inspector.structuredErrors=true"
