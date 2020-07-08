085429623a6d007a2119b7d8744b8dff715778f1
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-07-08 08:46:33 +0530

    add bmi starter files

--- bmi-calculator-flutter/ios/Flutter/flutter_export_environment.sh
+++ bmi-calculator-flutter/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/jaybhoyar/Documents/flutterSDK"
-export "FLUTTER_APPLICATION_PATH=/Users/jaybhoyar/projects/flutter/all projects/quizzler-flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jaybhoyar/projects/flutter/all projects/bmi-calculator-flutter"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
