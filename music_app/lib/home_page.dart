498d2093552be63c98ee2cfb55ea8c684531d62b
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-07-14 22:45:28 +0530

    add image assest

--- music_app/lib/home_page.dart
+++ music_app/lib/home_page.dart
@@ -9,13 +9,36 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple,
+      backgroundColor: Colors.pink,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          SizedBox(
-            height: 50.0,
+          SizedBox(height: 50.0),
+          Padding(
+            padding: EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
+                Text(
+                  'Playing Now',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                ),
+              ],
+            ),
           ),
+          SizedBox(height: 50.0),
           Expanded(
             child: Container(
               decoration: BoxDecoration(
@@ -24,6 +47,38 @@ class _MainPageState extends State<MainPage> {
                   topRight: Radius.circular(30.0),
                   topLeft: Radius.circular(30.0),
                 ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 20.0),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 20.0),
+                      child: Icon(
+                        Icons.favorite,
+                        color: Colors.pinkAccent,
+                        size: 30.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Center(
+                    child: Container(
+                      height: 250,
+                      width: 250,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey.shade100,
+                        borderRadius: BorderRadius.circular(200),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(200),
+                        child: Image.asset('images/'),
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
           )
