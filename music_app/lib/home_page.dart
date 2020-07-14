dd9d9ea31e3b5643d0448dd8eceb8a396e60903f
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-07-14 23:21:54 +0530

    add audio see

--- music_app/lib/home_page.dart
+++ music_app/lib/home_page.dart
@@ -9,11 +9,15 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink,
+      backgroundColor: Colors.deepPurple,
+      // appBar: AppBar(
+      //   title: Text('Playing Now'),
+
+      // ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          SizedBox(height: 50.0),
+          SizedBox(height: 40.0),
           Padding(
             padding: EdgeInsets.only(left: 20.0, right: 20.0),
             child: Row(
@@ -38,7 +42,7 @@ class _MainPageState extends State<MainPage> {
               ],
             ),
           ),
-          SizedBox(height: 50.0),
+          SizedBox(height: 40.0),
           Expanded(
             child: Container(
               decoration: BoxDecoration(
@@ -51,7 +55,7 @@ class _MainPageState extends State<MainPage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  SizedBox(height: 20.0),
+                  SizedBox(height: 15.0),
                   Align(
                     alignment: Alignment.topRight,
                     child: Padding(
@@ -63,21 +67,87 @@ class _MainPageState extends State<MainPage> {
                       ),
                     ),
                   ),
-                  SizedBox(height: 20.0),
+                  SizedBox(height: 15.0),
                   Center(
                     child: Container(
                       height: 250,
                       width: 250,
+                      padding: EdgeInsets.all(8.0),
                       decoration: BoxDecoration(
-                        color: Colors.blueGrey.shade100,
+                        color: Colors.deepPurple.shade100,
                         borderRadius: BorderRadius.circular(200),
                       ),
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(200),
-                        child: Image.asset('images/'),
+                        child: Image.asset(
+                          'images/sample.jpeg',
+                          fit: BoxFit.contain,
+                        ),
                       ),
                     ),
-                  )
+                  ),
+                  SizedBox(height: 25),
+                  Center(
+                    child: Text(
+                      'Lose It',
+                      style: TextStyle(
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Center(
+                    child: Text(
+                      'Flume ft. Vic Mensa',
+                      style: TextStyle(
+                          fontSize: 14.0, color: Colors.grey.shade600),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          '0:58',
+                          style: TextStyle(
+                            fontSize: 13.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '3:36',
+                          style: TextStyle(
+                            fontSize: 13.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Slider(
+                    value: 0.1,
+                    onChanged: (value) {},
+                    activeColor: Colors.red,
+                    inactiveColor: Colors.grey.shade500,
+                  ),
+                  SizedBox(height: 20.0),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30, right: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Icon(
+                          Icons.repeat,
+                          size: 30,
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
