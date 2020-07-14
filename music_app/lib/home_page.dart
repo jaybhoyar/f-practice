9d68c6f9d3e90a407453c786e9f9bfb936135514
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-07-14 20:47:48 +0530

    add intial styling

---
+++ music_app/lib/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(
+            height: 50.0,
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30.0),
+                  topLeft: Radius.circular(30.0),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
